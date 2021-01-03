5ef0d552b2bade15c08c52ab90cc139791ed900b
Ioana Chiorean <chiorean.ioana@gmail.com>
2021-01-03 15:45:44 +0200

    Day 3 both part 1 and 2
    For part 1 run only once the function with 1,3 as input

---
+++ day3.py
@@ -0,0 +1,27 @@
+# Using readlines()
+inputList = open("inputDay3.txt", "r")
+lines = inputList.readlines()
+allColumns = len(lines[0]) - 2
+
+
+def slopeTree(forest, tree):
+    if forest[tree] == "#":
+        return True
+    return False
+
+
+def slopeType(x, y):
+    nrTree = 0
+    columnSlope = 0
+    linePos = 0
+    for i in lines:
+        if linePos % x == 0:
+            if slopeTree(i, columnSlope):
+                nrTree += 1
+            columnSlope += y
+            if columnSlope > allColumns:
+                columnSlope = columnSlope - allColumns - 1
+        linePos += 1
+    return nrTree
+
+print( slopeType(1,1) * slopeType(1,3) * slopeType(1,5) *slopeType(1,7) *slopeType(2,1))
